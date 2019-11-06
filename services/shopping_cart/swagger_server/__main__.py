2b1e9f51b4e697ea418e299103bca98e0a4cadef
tobias <tobias@chainsecurity.com>
2019-11-06 22:55:26 +0100

    basic setup

--- services/shopping_cart/swagger_server/__main__.py
+++ services/shopping_cart/swagger_server/__main__.py
@@ -6,7 +6,11 @@ from swagger_server import encoder
 
 
 def main():
-    app = connexion.App(__name__, specification_dir='./swagger/')
+    options = {
+        "swagger_ui": True
+    }
+    app = connexion.App(__name__, specification_dir='./swagger/',
+                        options=options)
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Shopping cart microservice'})
     app.run(port=8080)
